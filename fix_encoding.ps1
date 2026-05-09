6760f08979e2edb0e60e05fad8ed4699c5b16f47
enigma <205631657+willseeee@users.noreply.github.com>
2026-05-09 21:10:00 +0530

    UPDATE

--- fix_encoding.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem -Path ".\" -Filter "*.html"
-foreach ($file in $files) {
-    $content = Get-Content -Raw -Path $file.FullName -Encoding UTF8
-    $newContent = $content.Replace("â–¼", "▼")
-    Set-Content -Path $file.FullName -Value $newContent -Encoding UTF8
-}
